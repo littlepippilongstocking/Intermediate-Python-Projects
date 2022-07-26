cc77021b419785b12b25dd0239984f8f50efd946
Dilyana Koleva <64352725+littlepippilongstocking@users.noreply.github.com>
2022-07-26 13:17:08 +0100

    Updated the code to make the output easier to read

--- weather.py
+++ weather.py
@@ -14,8 +14,16 @@ def get_weather():
     API_key = ''  # Add your API key here
     city = input("Please enter a city: ")
     base_url = "http://api.openweathermap.org/data/2.5/weather?&q=" + city + "&appid=" + API_key
-    weather_data = requests.get(base_url).json()
-    pprint(weather_data)
+    weather_data = requests.get(base_url).json()['main']
+    temp = weather_data['temp']
+    feels_like = weather_data['feels_like']
+    temp_min = weather_data['temp_min']
+    temp_max = weather_data['temp_max']
+    pressure = weather_data['pressure']
+    humidity = weather_data['humidity']
+    print(f"Welcome to {city}. The temperature here today is {temp} degrees, but it feels like {feels_like} degrees.\n"
+          f"The maximum temperature would be {temp_max} degrees and the minimum : {temp_min} degrees.\n"
+          f"The pressure and humidity are respectively {pressure} hPa and {humidity}%.")
 
 
 if __name__ == '__main__':
